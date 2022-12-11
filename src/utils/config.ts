b48c495d1637d679c9c0ff236bb2c80f696cada1
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2022-12-11 20:23:05 +0200

    feat: create env template and config file

---
+++ src/utils/config.ts
@@ -0,0 +1,12 @@
+export const config = {
+  host: process.env.REACT_APP_HOST,
+  zegoCloudAppId: process.env.REACT_APP_ZEGOCLOUD_APP_ID,
+  zegoCloudServerSecret: process.env.REACT_APP_ZEGOCLOUD_SERVER_SECRET,
+  firebaseApiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  firebaseAuthDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  firebaseProjectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  firebaseStorageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  firebaseMessagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  firebaseAppId: process.env.REACT_APP_FIREBASE_APP_ID,
+  firebaseMeasurmentId: process.env.REACT_APP_FIREBASE_MEASURMENT_ID,
+};
--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -2,6 +2,7 @@
 import { getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import { collection, getFirestore } from 'firebase/firestore';
+import { config } from './config';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,13 +10,13 @@ import { collection, getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAnF8AZ4g8apCn0Fl_mb-kSOKNHzxv4GYc',
-  authDomain: 'video-chat-c8c87.firebaseapp.com',
-  projectId: 'video-chat-c8c87',
-  storageBucket: 'video-chat-c8c87.appspot.com',
-  messagingSenderId: '694373989716',
-  appId: '1:694373989716:web:7c798afa2c064504099cc1',
-  measurementId: 'G-C4VG2JHDF9',
+  apiKey: config.firebaseApiKey,
+  authDomain: config.firebaseAuthDomain,
+  projectId: config.firebaseProjectId,
+  storageBucket: config.firebaseStorageBucket,
+  messagingSenderId: config.firebaseMessagingSenderId,
+  appId: config.firebaseAppId,
+  measurementId: config.firebaseMeasurmentId,
 };
 
 // Initialize Firebase
